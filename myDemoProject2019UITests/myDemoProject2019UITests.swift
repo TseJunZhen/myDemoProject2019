d843cf4b1a1359e1b3a2c53889bc15c7b67e43b3
Tse Jun Zhen <victor@Tses-MacBook-Pro.local>
2019-01-10 23:31:00 +0800

    first commit by UItableview-demo

---
+++ myDemoProject2019UITests/myDemoProject2019UITests.swift
@@ -0,0 +1,34 @@
+//
+//  myDemoProject2019UITests.swift
+//  myDemoProject2019UITests
+//
+//  Created by Tse Jun Zhen on 10/01/2019.
+//  Copyright © 2019 TseJunZhen. All rights reserved.
+//
+
+import XCTest
+
+class myDemoProject2019UITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
